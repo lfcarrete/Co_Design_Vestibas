9046e14db622541e9cea7de3445383a0cf3fefc5
Luis Filipe Carrete <lfcsanchez@live.com>
2019-11-13 07:55:37 -0300

    .

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,10 +8,12 @@ document.addEventListener('DOMContentLoaded', function() {
    menu_img.addEventListener("click", function(){
          if (open == 0){
             menu.style.display = "block";
+            console.log("open");
             open += 1;
          }
          else if (open ==1){
             menu.style.display = "none";
+            console.log("close");
             open -= 1;
          }
    })
