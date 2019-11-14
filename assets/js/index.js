dbac812c18d1114ab092d0c29e6a73138c1a4fe0
Luis Filipe Carrete <lfcsanchez@live.com>
2019-11-14 14:46:07 -0300

    .

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-   let menu_img = document.querySelector(".menu_img")
-   let menu = document.querySelector(".menu")
-
    let open = 0
 
-   menu_img.addEventListener("click", function(){
+   document.querySelector(".menu_img").addEventListener("click", function(){
          if (open == 0){
-            menu.style.display = "block";
+            document.querySelector(".menu").style.display = "block";
             console.log("open");
             open += 1;
          }
          else if (open ==1){
-            menu.style.display = "none";
+            document.querySelector(".menu").style.display = "none";
             console.log("close");
             open -= 1;
          }
-   })
+   });
 })
 
 
